3b7db28b036d838638759c544fd7bb669d4ee4a0
boboRay <ray820821@gmail.com>
2016-04-04 17:09:30 +0800

    [HW1] Add script to download data

---
+++ hw1/setup.sh
@@ -0,0 +1,7 @@
+wget http://www.csie.ntu.edu.tw/~r03922152/WM16/CIRB010.tar.gz
+wget http://www.csie.ntu.edu.tw/~r03922152/WM16/model.tar.gz
+wget http://www.csie.ntu.edu.tw/~r03922152/WM16/queries.tar.gz
+
+tar zxvf CIRB010.tar.gz
+tar zxvf model.tar.gz
+tar zxvf queries.tar.gz
