ca063b657df411576e146b9457b3145a5747a011
boboRay <ray820821@gmail.com>
2016-04-09 00:33:31 +0800

    [hw1] add rm in setup.py

--- hw1/setup.sh
+++ hw1/setup.sh
@@ -5,3 +5,7 @@ wget http://www.csie.ntu.edu.tw/~r03922152/WM16/queries.tar.gz
 tar zxvf CIRB010.tar.gz
 tar zxvf model.tar.gz
 tar zxvf queries.tar.gz
+
+rm queries.tar.gz
+rm model.tar.gz
+rm CIRB010.tar.gz 
